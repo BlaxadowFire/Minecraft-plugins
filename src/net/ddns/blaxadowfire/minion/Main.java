c404cb1e1b917c3d1d5ee628967e0258f65b29b1
BlaxadowFire <nando.kools116325@student.leeuwenborgh.nl>
2018-06-19 12:27:47 +0200

    Added onEnable in main, and created a starting message.
    included org.bukkit.Bukkit;
    included org.bukkit.ChatColor;

--- src/net/ddns/blaxadowfire/minion/Main.java
+++ src/net/ddns/blaxadowfire/minion/Main.java
@@ -1,6 +1,13 @@
 package net.ddns.blaxadowfire.minion;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
+    @Override
+    public void onEnable()
+    {
+        Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "Minion Plugin is loading");
+    }
 }
