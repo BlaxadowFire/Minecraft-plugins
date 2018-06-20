55c750421233db08122bc7a54e8b3a0fefae602c
BlaxadowFire <nando.kools116325@student.leeuwenborgh.nl>
2018-06-20 12:58:33 +0200

    Added Minion class

---
+++ src/net/ddns/blaxadowfire/minion/Minion.java
@@ -0,0 +1,14 @@
+package net.ddns.blaxadowfire.minion;
+
+import org.bukkit.event.Listener;
+
+public class Minion implements Listener {
+
+    public Minion(){
+
+    }
+    public void Mining(){
+
+    }
+
+}
