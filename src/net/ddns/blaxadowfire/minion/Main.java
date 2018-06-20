6d175ceff211e37edc2a54c9e5eb40f1ca72e56c
BlaxadowFire <nando.kools116325@student.leeuwenborgh.nl>
2018-06-20 12:54:08 +0200

    re-added removed lines in main

--- src/net/ddns/blaxadowfire/minion/Main.java
+++ src/net/ddns/blaxadowfire/minion/Main.java
@@ -8,6 +8,9 @@ public class Main extends JavaPlugin {
     @Override
     public void onEnable()
     {
+        Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "Minion Plugin is loading");
+        //this.getServer().getPluginManager().registerEvents(new SpawnEntity(), this);
+
         getCommand("monster").setExecutor(new SpawnEntity(this));
         getCommand("givesword").setExecutor(new SpawnEntity(this));
         getCommand("givearmor").setExecutor(new SpawnEntity(this));
